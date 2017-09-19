bf562039fb9431ef01083ad5b8c75805e97ff2d4
Malik Hemphill <lazaryr@gmail.com>
2017-09-19 16:01:54 -0400

    added leave command

---
+++ commands/leave.js
@@ -0,0 +1,24 @@
+exports.run = (bot, message, args, about, rn, sorrows, convertTime, displayWords, checkWord, singleWord, prefix, botUptime, banned, checkID, fs, newGuildHook, blacklistHook, safe) => {
+    const currentGuild = message.guild;
+    const currentGuildID = message.guild.id;
+    const messageCount = message.content.split(` `).length;
+    let serverID = message.content.split(` `)[2];
+    
+    if (message.author.id !== "303105492572569600") {
+        return message.channel.send('You don\'t have permission you :chicken:!');
+    }
+    if (currentGuildID != '299853081578045440') {
+        return message.channel.send('Not in this server!');
+    }
+    
+    if (messageCount != 3) {
+        return message.channel.send('Brother man, you need to add a Guild ID for me to leave.');
+    } else {
+        for (let guild of bot.guilds) {
+            if (guild.includes(serverID)) {
+                message.channel.send(`I have left the guild: \`${guild[1].name}\``);
+                return guild[1].leave();
+            }
+        }
+    }
+}
